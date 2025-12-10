00f05225a7b3726e763322dbf2e80bde073a598a
taufiq-r <taufiiqurahman@gmail.com>
2025-12-10 11:24:58 +0700

    Fix Systemd Logging using promtail, loki

--- install_docker.sh
+++ install_docker.sh
@@ -39,7 +39,7 @@ installDocker(){
    sudo apt install -y ca-certificates curl gnupg lsb-release
    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   
-   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list> /dev/null
+   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list> /dev/null
    echo "Update package..."
    sudo apt update
    echo "Installing Docker..."
